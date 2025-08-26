48a9255bf266db6f212d30f7c62629401a590276
Roshan J <sam675128@gmail.com>
2025-08-26 12:03:06 +0530

    Add files via upload

---
+++ Hackathon_Assignment2/Ques1MyWords/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace SampleWebFormsApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var path = Server.MapPath("//Images");
+            var data = new ProductDatabase().GetAllProducts(path);
+            Application["AllProducts"] = data;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
