bde9e98002f2b357cb6424259964dfb5314f756a
Roshan J <sam675128@gmail.com>
2025-09-18 12:37:51 +0530

    Add files via upload

---
+++ YumCart/Models/food-item.ts
@@ -0,0 +1,9 @@
+export interface FoodItem {
+    ItemId : number,
+    ItemName : string,
+    ItemImg : string,
+    ItemQuantity : number,
+    ItemCost : number,
+    RestaurantName : string,
+    RestaurantLocation : string
+}
